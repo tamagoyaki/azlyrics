334a6c62c0670e5ea0dc39a14789239d81a30661
tamagoyaki <tamagoyaki@users.noreply.github.com>
2015-08-28 14:47:43 +0900

    modified

--- azlrcs-dl.sh
+++ azlrcs-dl.sh
@@ -3,9 +3,27 @@
 # USAGE:
 #
 #   $ cat audio.cdindex | ./azlrcs-url.sh | ./azlrcs-dl.sh
+#   $ echo http://www.azlyrics.com/lyrics/hoge/gero.html | ./azlrcs-dl.sh
 #
 
-todo=`awk '{print "wget -U Mozilla " $1 " -O " $2}'`
+
+#
+# $1, URL
+# $2, output-filename
+#
+# If no output-filename specified, generate it from URL.
+#
+todo=`awk '{
+  oname = $2;
+
+  if ("" == oname) {
+    oname = $1;
+    sub(".*/", "", oname);
+  }
+
+  print "wget -U Mozilla " $1 " -O " oname
+}'`
+
 
 IFS=$'\n'
 
